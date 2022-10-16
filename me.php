cd355d4e68d604195ae77d45ff162d5dc7d4e7bd
Michaelwahyu <89862620+Michaelwahyu@users.noreply.github.com>
2022-10-16 18:15:26 +0700

    Add files via upload

---
+++ me.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+    <!-- Third party plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="script" href="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+    <title>Welcome To My Website</title>
+</head>
+
+<body id="page-top">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Michael Wahyu Rizky Setiawan</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="about.php">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="services.php">Services</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="portfolio.php">Message me</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--nav bar ended-->
+    <div class="container-sm text-center">
+        <div class="container-fluid" id="part_one_one">
+            <h3>Daftar Riwayat Hidup</h3>
+            <p style=color:#fff;>Menjadi salah satu diantara ahli bidang Programming dengan pengembangan
+            di bidang Website dan Mobile.
+            Fokus utama yang berkaitan dengan<i> Web Developer, Web Designer dan Database</i>. Oleh karenanya
+            minat yang saya ambil adalah bidang IT. Untuk menempuh itu tentu ditunjang skill teknikal, terutama
+            dalam hal Website, database programming dan semua yang berkaitan dengan Programming.</p>
+            <h3>Summary</h3>
+            <p><em>Terimakasih kepada para dosen dan tenaga pengajar yang telah mengajari kami Mengenai koding
+            dan Bahasa Pemrograman. Kemampuan ini akan kami bawa ke dunia kerja.</em></p>
+            <p><em>Menyelesaikan setiap permasalahan dengan coding adalah suatu kesenangan tersendiri bagi
+            seorang programmer dan harus belajar bersabar dalam menemukan setiap permasalahan atau Error yang
+            dihadapi.</em></p>
+    </div>
+    <h3 class="resume-tittle" style=color:#fff;>Education</h3>
+    <table class="table table-hover">
+        <thead>
+            <tr style=color:#fff;>
+                <th scope="col">#</th>
+                <th scope="col">Pendidikan</th>
+                <th scope="col">Tahun</th>
+                <th scope="col">Alamat</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr style=color:#fff;>
+                <th scope="row">1</th>
+                <td>SD Negeri Serpong 2</td>
+                <td>2008 - 2014</td>
+                <td><p><em>Jl. Kavling Serpong No.Rt.01/04, Serpong, Kec. Serpong, Kota Tangerang Selatan, Banten 15310</em></p></td>
+            </tr>
+            <tr style=color:#fff;>
+                <th scope="row">2</th>
+                <td>SMP PGRI 35 Serpong</td>
+                <td>2014 - 2017</td>
+                <td><p><em>Jalan Raya, RT.04/RW.05, Serpong, Serpong Sub-District, South Tangerang City, Banten 15310</em></p></td>
+            </tr>
+            <tr style=color:#fff;>
+                <th scope="row">3</th>
+                <td>SMK Negeri 1 Kota Tangerang Selatan</td>
+                <td>2017 - 2020</td>
+                <td><p><em>Gg. Waru No.59, Ciater, Kec. Serpong, Kota Tangerang Selatan, Banten 15310</em></p></td>
+            </tr>
+            <tr style=color:#fff;>
+                <th scope="row">4</th>
+                <td>Universitas Pembangunan Jaya</td>
+                <td>2020 - Now</td>
+                <td><p><em>Jl. Cendrawasih Raya, Sawah Baru, Ciputat, South Tangerang City, Banten 15413</em></p></td>
+            </tr>
+        </tbody>
+    </table>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+    <!-- Third party plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+
+
+
+
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/custome.js"></script>
+    <script type="text/javascript" src="js/particles.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
